4408e7d39c8635ac6c955c53b1a42f414e2d0fb9
Shikanime Deva <22115108+Shikanime@users.noreply.github.com>
2022-04-13 13:05:14 +0200

    Remove Home Manager

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     devShell = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix (system:
       import ./shell.nix { pkgs = import nixpkgs { inherit system; }; }
     );
